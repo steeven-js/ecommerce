3a9b79da390dfdb7e89ea893a662d67f64e1a350
steeven-js <jacques.steeven@gmail.com>
2024-04-29 04:50:20 -0400

    Mettre à jour la configuration Firebase pour utiliser les variables d'environnement

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRpw3ZS9N3c5479gTsL5LSOqES01--yNs",
-  authDomain: "ecommerce-db27e.firebaseapp.com",
-  projectId: "ecommerce-db27e",
-  storageBucket: "ecommerce-db27e.appspot.com",
-  messagingSenderId: "578323054723",
-  appId: "1:578323054723:web:ec3e0e02b59d9cbc095bb6",
-  measurementId: "G-JT99W0RWG1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
